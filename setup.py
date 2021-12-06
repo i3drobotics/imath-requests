5e7e4a96f8bd2a548feb9b6f056780d16a3c5118
Ben <benknight135@gmail.com>
2021-12-06 12:11:29 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='imath-requests',
+      version='0.1',
+      description='Communication with the iMath data platform REST API',
+      url='http://github.com/i3drobotics/imath-requests',
+      author='Industrial 3D Robotics',
+      author_email='bknight@i3drobotics.com',
+      license='MIT',
+      packages=['imath-requests'],
+      zip_safe=False)
