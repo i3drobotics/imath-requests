0a2cd09171cafe9a79683646fd3f4dc1dbd2bdda
Ben <benknight135@gmail.com>
2021-12-08 13:23:03 +0000

    working unittest for server

--- tests/test_server.py
+++ tests/test_server.py
@@ -10,84 +10,49 @@ import tempfile
 from imath_requests.server import create_app
 
 
-@pytest.fixture
-def client():
-    app, api = create_app({'TESTING': True})
+class TestPartDataEndpoint(unittest.TestCase):
+    """
+    Unit testing for part data endpoint of app in Server module.
 
-    with app.test_client() as client:
-        with app.app_context():
-            pass
-        yield client
+    """
+    def setUp(self):
+        app, api = create_app({'TESTING': True})
+        self.client = app.test_client()
 
+    def test_part_data_get(self):
+        self.client.get('/part_data', follow_redirects=True)
 
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
-
-@pytest.fixture
-def runner(app):
-    return app.test_cli_runner()
-
-
-def test_config():
-    assert not create_app().testing
-    assert create_app({'TESTING': True}).testing
-
-
-def test_index(client):
-    response = client.get('/')
-    print(response)
-
-
-# class TestPartDataEndpoint(unittest.TestCase):
-#     """
-#     Unit testing for part data endpoint in server module.
-
-#     """
-#     def part_data_post(self, data):
-#         return client.post('/part_data', data=data, follow_redirects=True)
-
-#     def part_data_get(self):
-#         return client.get('/part_data', follow_redirects=True)
-
-#     def test_part_data(self):
-#         part_data = {
-#             "timestamp": 1516193959559,
-#             "part_id": "Part1234",
-#             "source": "Camera_Control_PC_Garret",
-#             "part_data": [
-#                 {
-#                     "key": "steel_grade",
-#                     "value": "Grade01"
-#                 },
-#                 {
-#                     "key": "heat_number",
-#                     "value": "C1234566"
-#                 },
-#                 {
-#                     "key": "rolling_schedule",
-#                     "value": "Schedule1"
-#                 },
-#                 {
-#                     "key": "analysis",
-#                     "value": [
-#                         {
-#                             "key": "C",
-#                             "value": "0.2"
-#                         },
-#                         {
-#                             "key": "Mn",
-#                             "value": "0.02"
-#                         }
-#                     ]
-#                 }
-#             ]
-#         }
-#         rv = self.part_data_post(part_data)
-#         # TODO assert post was succesfull
-#         # assert b'test' in rv.data
-
-#         rv = self.part_data_get()
-#         # TODO assert get was succesfull
-#         # assert b'test' in rv.data
+    def test_part_data_post(self):
+        part_data = {
+            "timestamp": 1516193959559,
+            "part_id": "Part1234",
+            "source": "Camera_Control_PC_Garret",
+            "part_data": [
+                {
+                    "key": "steel_grade",
+                    "value": "Grade01"
+                },
+                {
+                    "key": "heat_number",
+                    "value": "C1234566"
+                },
+                {
+                    "key": "rolling_schedule",
+                    "value": "Schedule1"
+                },
+                {
+                    "key": "analysis",
+                    "value": [
+                        {
+                            "key": "C",
+                            "value": "0.2"
+                        },
+                        {
+                            "key": "Mn",
+                            "value": "0.02"
+                        }
+                    ]
+                }
+            ]
+        }
+        self.client.post('/part_data', data=part_data, follow_redirects=True)
